21b5b6d8a457e1ad37e555e38c44cdb932ea8641
dadelosreyes <davedelosreyes26@gmail.com>
2026-03-10 14:02:03 +0800

    test

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       "/api": {
-        target: "http://0.0.0.0:5188",
+        target: "https://localhost:7154",
         secure: false,
         changeOrigin: true,
       },
